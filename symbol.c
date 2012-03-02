4c6626dc0c477e004e7eec31e562244082776c77
Peter Backman <ptr.bckmn@gmail.com>
2012-03-03 00:32:24 +0100

    forgot to add string

--- symbol.c
+++ symbol.c
@@ -1,12 +1,14 @@
 #include "symbol.h"
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 
 object_t *sym_create(const char *sym) {
   symbol_t *new_sym = (symbol_t *)malloc(sizeof(symbol_t));
   new_sym->header.type = O_SYMBOL;
   new_sym->header.members = NULL;
-
+  new_sym->sym = strdup(sym);
+  
   return (object_t *)new_sym;
 }
 
