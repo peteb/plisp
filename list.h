31b324d8eedc20c2fbf45ac5f327445b5d0bfc22
Peter <ptr.bckmn@gmail.com>
2012-03-05 14:07:50 +0100

    Update list.h

--- list.h
+++ list.h
@@ -5,10 +5,13 @@
 
 object_t *
 lst_cons(object_t *o1, object_t *o2);
+
 object_t *
 lst_cons_lazy(object_t *o1, object_t *o2);
+
 object_t *
 lst_first(object_t *tup);
+
 object_t *
 lst_second(object_t *tup);
 
