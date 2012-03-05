ca353778668230f286e9f9c2eed05ebd37c51ae6
Peter <ptr.bckmn@gmail.com>
2012-03-05 14:07:35 +0100

    Update list.h

--- list.h
+++ list.h
@@ -3,9 +3,13 @@
 
 #include "object.h"
 
-object_t *lst_cons(object_t *o1, object_t *o2);
-object_t *lst_cons_lazy(object_t *o1, object_t *o2);
-object_t *lst_first(object_t *tup);
-object_t *lst_second(object_t *tup);
+object_t *
+lst_cons(object_t *o1, object_t *o2);
+object_t *
+lst_cons_lazy(object_t *o1, object_t *o2);
+object_t *
+lst_first(object_t *tup);
+object_t *
+lst_second(object_t *tup);
 
 #endif // !PLISP_LIST_H
